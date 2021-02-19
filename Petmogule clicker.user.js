31c68820a480197ed6ac1f1edc111484e8ef67d1
007roy <troy.bennett@gmail.com>
2021-02-19 11:51:56 -0800

    #lvlbt disable reload

--- Petmogule clicker.user.js
+++ Petmogule clicker.user.js
@@ -1,65 +1,73 @@
 // ==UserScript==
 // @name         Petmogule clicker
 // @namespace    https://violentmonkey.github.io
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://petmogul-2-0.com/profile.php?id=*
 // @require      http://code.jquery.com/jquery-latest.min.js
-// @grant        none
+// @grant        GM_getValue
 // ==/UserScript==
+   
+var jQuery = window.jQuery;
 
-(function(){
-    const me = 143;
-    
-    var jQuery = window.jQuery;
-    function buy(){
-        if(!!document.getElementById("pubt")){
-            document.getElementById("pubt").click();
-        }
+function buy(){
+    if(!clickerOn) return;
+    if(!!document.getElementById("pubt")){
+        document.getElementById("pubt").click();
     }
+}
 
-    function quickCan(){
-        var scash = document.getElementById("curentcash").getElementsByTagName("SPAN")[0].getAttribute("title").split(",")[0].substr(1);
-        var icash = parseInt(scash);
-        if(icash < 6) document.getElementById("quickcan").click();
-        if(icash == 17){
-            document.getElementById("pd").click();
-            var drop = document.getElementById("dropvalue");
-            drop.selectedIndex = "9";
-            drop.dispatchEvent(new Event('change', {bubbles: true }));
-        }
+function quickCan(){
+    if(!clickerOn) return;
+    var scash = document.getElementById("curentcash").getElementsByTagName("SPAN")[0].getAttribute("title").split(",")[0].substr(1);
+    var icash = parseInt(scash);
+    if(icash < 9) document.getElementById("quickcan").click();
+    if(icash == 17){
+        document.getElementById("pd").click();
+        var drop = document.getElementById("dropvalue");
+        drop.selectedIndex = "9";
+        drop.dispatchEvent(new Event('change', {bubbles: true }));
     }
+}
 
-    function level(){
-        if(!!document.getElementById("lvlbt1") && !ibuying)
-        {
-            document.getElementById("quickcan").click();
-            document.getElementById("lvlbt1").click();
-            ibuying = true;
-            setTimeout(() => {document.getElementById("lvlbt").click(); ibuying = false;},500);
-        }
+function level(){
+    let id = GM_getValue("LEVEL_ID", 0)
+    if(!clickerOn) return;
+    var urlParams = new URLSearchParams(window.location.search);
+    if(id!=urlParams.get('id')) return;
+    if(!!jQuery("#lvlbt1") && !ibuying)
+    {
+        jQuery("#quickcan").click();
+        jQuery("#lvlbt1").click();
+        ibuying = true;
+        setTimeout(() => {jQuery("#lvlbt").click(); ibuying = false;},500);
     }
-    var ibuying = false;
+    if(jQuery('#lvlbt').prob('disabled')) reloadPage();
+}
 
-    setInterval(buy, 100);
-    setInterval(quickCan,1000);
+function reloadPage(){
+    location.reload();
+}
 
-    setInterval(function(){
-        var urlParams = new URLSearchParams(window.location.search);
-        var id = urlParams.get('id');
-        if(id==me) level();
-    },1000);
+var ibuying = false;
+var clickerOn = true;
   
-  
-  var zNode = document.createElement ('div');
-  zNode.innerHTML = '<button id="myButton" type="button">Clicker Off</button><br>';
-  zNode.setAttribute('id', 'myContainer');
-  jQuery("#playercash").after(zNode);
-  jQuery("#myButton").on("click", ClickerPowerOn);
-  function ClickerPowerOn(zEvent){
-    console.log('clicker');
-      jQuery("#myButton").text(function(i, text){return text === "Clicker On" ? "Clicker Off" : "Clicker On";});
-  }
+setInterval(buy, 100);
+setInterval(quickCan,1000);
+setInterval(level(),1000);
+setInterval(reloadPage, GM_getValue("REFRESH_INTERVAL", 600000));
 
-})();
+var zNode = document.createElement ('div');
+zNode.innerHTML = '<button id="myButton" type="button">Clicker On</button><br>';
+zNode.setAttribute('id', 'myContainer');
+jQuery("#playercash").after(zNode);
+jQuery("#myButton").on("click", ClickerPowerOn);
+
+function ClickerPowerOn(zEvent){
+    jQuery("#myButton").text(function(i, text){return text === "Clicker On" ? "Clicker Off" : "Clicker On";});
+    clickerOn = clickerOn ? false : true;
+}
+
+
+// #lvlbt
