03a4ba99f05252f7fc4ab05016c4fe20b16347a5
007roy <troy.bennett@gmail.com>
2021-02-20 04:33:38 +0000

    got autolvl and autobuy working

--- Petmogule clicker.user.js
+++ Petmogule clicker.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Petmogule clicker
 // @namespace    https://violentmonkey.github.io
-// @version      0.8
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://petmogul-2-0.com/profile.php?id=*
 // @require      http://code.jquery.com/jquery-latest.min.js
 // @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
    
 var jQuery = window.jQuery;
@@ -17,10 +18,12 @@ function buy(){
         document.getElementById("pubt").click();
     }
 }
-
+function getCash(){
+  var scash = document.getElementById("curentcash").getElementsByTagName("SPAN")[0].getAttribute("title").split(",")[0].substr(1);
+  return parseInt(scash);
+}
 function quickCan(){
-    var scash = document.getElementById("curentcash").getElementsByTagName("SPAN")[0].getAttribute("title").split(",")[0].substr(1);
-    var icash = parseInt(scash);
+    var icash = getCash();
     if(icash < 9) document.getElementById("quickcan").click();
     if(icash == 17){
         document.getElementById("pd").click();
@@ -33,16 +36,17 @@ function quickCan(){
 function level(){
     //let id = GM_getValue("LEVEL_ID", 0)
     if(!jQuery("#autoLevelCheck").prop("checked")) return;
-    var urlParams = new URLSearchParams(window.location.search);
-    if(id!=urlParams.get('id')) return;
-    if(!!jQuery("#lvlbt1") && !ibuying)
+    //var urlParams = new URLSearchParams(window.location.search);
+    //if(id!=urlParams.get('id')) return;
+    if(jQuery("#lvlbt1").length == 1 && !ibuying)
     {
-        jQuery("#quickcan").click();
+        console.log('beep');
+        if(getCash() < 5) jQuery("#quickcan").click();
         jQuery("#lvlbt1").click();
         ibuying = true;
         setTimeout(() => {jQuery("#lvlbt").click(); ibuying = false;},500);
     }
-    if(jQuery('#lvlbt').is('[disabled=disabled]')) reloadPage();
+    if(jQuery('#lvlbt').prop('disabled')) reloadPage();
 }
 
 function reloadPage(){
@@ -50,24 +54,48 @@ function reloadPage(){
 }
 
 function flipBoard(){
+    if(!jQuery("#crawlBoardCheck").prop("checked")) return;
     jQuery('#pubboard > div:contains("LInk")').find("a").each(function(index){
-        console.log($(this).attr('href'));
+        let id = jQuery(this).attr('href').split('=')[1]
+        var urlParams = new URLSearchParams(window.location.search);
+        if(id!=urlParams.get('id')) jQuery(this).click();
     });
 }
 
 var ibuying = false;
-var clickerOn = true;
   
 setInterval(buy, 10);
 setInterval(quickCan,1000);
-setInterval(level(),1000);
+setInterval(level,1000);
 setInterval(reloadPage, GM_getValue("REFRESH_INTERVAL", 600000));
+
+setInterval(flipBoard, 10000);
 
 var zNode = document.createElement ('div');
 zNode.innerHTML = `
    <input id="autoBuyCheck" type="checkbox"><label for="autoBuyCheck">Auto Buy</label>
    <input id="autoLevelCheck" type="checkbox"><label for="autoLevelCheck">Auto Level</label>
+   <input id="crawlBoardCheck" type="checkbox"><label for="crawlBoard">Crawl Board</label>
 `;
+
 zNode.setAttribute('id', 'clickerContainer');
-jQuery('#clickerContainer').css( "border", "3px double red" );
+zNode.setAttribute('style','border:3px double red');
 jQuery("#playercash").after(zNode);
+
+jQuery("#autoBuyCheck").prop("checked",GM_getValue("AUTO_BUY",false));
+jQuery("#autoBuyCheck").on("click", function(){
+    GM_setValue("AUTO_BUY",jQuery("#autoBuyCheck").prop("checked"));
+});
+jQuery("#autoLevelCheck").prop("checked",GM_getValue("AUTO_LEVEL",false));
+jQuery("#autoLevelCheck").on("click", function(){
+    GM_setValue("AUTO_LEVEL",jQuery("#autoLevelCheck").prop("checked"));
+    GM_setValue("CRAWL", false);
+});
+jQuery("#crawlBoardCheck").prop("checked",GM_getValue("CRAWL",false));
+jQuery("#crawlBoardCheck").on("click", function(){
+    GM_setValue("CRAWL",jQuery("#crawlBoardCheck").prop("checked"));
+    GM_setValue("AUTO_LEVEL",false);
+});
+
+
+
