64f0c70fad62de475f080ad7811fc64b12f5e0e4
007roy <troy.bennett@gmail.com>
2021-02-20 05:28:56 +0000

    inc ver

--- Petmogule clicker.user.js
+++ Petmogule clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Petmogule clicker
 // @namespace    https://violentmonkey.github.io
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://petmogul-2-0.com/profile.php?id=*
